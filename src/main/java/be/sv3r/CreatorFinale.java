efb3adbd3304de8d901468950ea6fee61c316732
sv3r <infosv3r@gmail.com>
2024-08-21 17:58:31 +0200

    initial commit

---
+++ src/main/java/be/sv3r/CreatorFinale.java
@@ -0,0 +1,14 @@
+package be.sv3r;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
+public class CreatorFinale extends JavaPlugin {
+    public static final Logger LOGGER = LoggerFactory.getLogger("creator-finale");
+
+    @Override
+    public void onEnable() {
+    }
+}
